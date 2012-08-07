22b6da747215ee02723fee0f66d99fd7e38d11ef
Alexander Auld <alexander.auld@hyperisland.se>
2012-08-07 15:46:43 +0200

    finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   before_filter :signed_in_user, only: [:index, :edit, :update]
   before_filter :correct_user,   only: [:edit, :update]
+  before_filter :admin_user,     only: :destroy
 
       def index
      @users = User.paginate(page: params[:page])
